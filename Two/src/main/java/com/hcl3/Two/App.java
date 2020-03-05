7a0e2bd5802f17b8bd39a401fd431b918db096ae
cisco <cisco@cisco-ThinkPad-T430>
2020-03-05 18:17:29 +0530

    new commit
    Change-Id: I5371149c318686a9818a53b3f5236186b9c5b536

---
+++ Two/src/main/java/com/hcl3/Two/App.java
@@ -0,0 +1,14 @@
+package com.hcl3.Two;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("rajesh");
+    }
+}
